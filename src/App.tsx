f157d4e3746c34bbec99a335f486ab70603eb06e
taniguchi-kakeru <jack.magatcha@gmail.com>
2023-04-24 18:52:52 +0900

    日付をシード値として問題を生成するロジックの実装（４つの数字の並びもシード値による）

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,20 @@ import './App.css';
 import Header from './components/header/Header';
 import HowToPlayDialog from './components/how_to_play_dialog/HowToPlayDialog';
 import MakeTen from './components/make_ten/MakeTen';
-import { StorageData } from './core/types';
+import { ProblemInfo, StorageData } from './core/types';
+import { problemList } from './assets/problem/problemList';
+import Chance from 'chance';
 
 /**
  * アプリケーションコンポーネント
  * @returns アプリケーションコンポーネント
  */
 function App() {
+  const problemCount: number = 5; // 出題する問題数
+
+  // 問題情報リスト
+  const [problemInfoList, setProblemInfoList] = useState<ProblemInfo[]>();
+
   // 問題の数字配列[4]
 	const [problemNumbers, setProblemNumbers] = useState<number[]>([0, 0, 0, 0]);
 
@@ -24,17 +31,25 @@ function App() {
 	 */
 	useEffect(() => {
     const today = getNowDate(); // 今日の日付を取得
-    // 今日既にページを開いていた場合
-    if (localStorage.getItem(StorageData.OpenPageDate) === today) {
-      console.log('今日は既に開いてます。');
-    }
-    // 今日始めてページを開いた場合
-    else {
+    // 今日始めてページを表示した場合
+    if (localStorage.getItem(StorageData.OpenPageDate) !== today) {
       localStorage.setItem(StorageData.OpenPageDate, today); // ストレージに今日の日付を格納
       setIsInitDisplay(true); // 遊び方ダイアログ初期表示フラグON
       changeHowToPlayDialog(true); // 遊び方ダイアログを表示
     }
-    createProblem(); // TODO: 問題の生成
+    const problemInfos = createProblem(today);
+
+    // 出題する問題情報リストに格納
+    setProblemInfoList(problemInfos);
+
+    const newProblemNumbers = [...problemNumbers]; // 配列の値渡し
+    newProblemNumbers[0] = problemInfos[0].problem[0];
+    newProblemNumbers[1] = problemInfos[0].problem[1];
+    newProblemNumbers[2] = problemInfos[0].problem[2];
+    newProblemNumbers[3] = problemInfos[0].problem[3];
+
+    // 問題の更新
+    setProblemNumbers(newProblemNumbers);
 	}, []);
 
   /**
@@ -59,17 +74,50 @@ function App() {
 
   /**
    * 問題の生成
-   * TODO: 仮実装
+   * @param {string} today - 今日の日付(YYYMMDD)
+   * @returns {ProblemInfo[]} 問題情報配列
    */
-  const createProblem = (): void => {
-    const newProblemNumbers = [...problemNumbers]; // 配列の値渡し
-    newProblemNumbers[0] = Math.floor(Math.random() * 8); // TODO: 0~9までの乱数生成
-    newProblemNumbers[1] = Math.floor(Math.random() * 8); // TODO: 0~9までの乱数生成
-    newProblemNumbers[2] = Math.floor(Math.random() * 8); // TODO: 0~9までの乱数生成
-    newProblemNumbers[3] = Math.floor(Math.random() * 8); // TODO: 0~9までの乱数生成
+  const createProblem = (today: string): ProblemInfo[] => {
+    // 今日の日付をシード値として乱数生成用インスタンを生成
+    const chance = new Chance(today);
+    const problemListIndexs: number[] = []; // 乱数で生成する問題のindexリスト
+    const problemOrderList: number[][] = []; // 4つの数字の出題順リスト
 
-    // 問題の更新
-    setProblemNumbers(newProblemNumbers);
+    // 出題する問題数分乱数を生成
+    for (let i = 0; i < problemCount; i++) {
+      // 問題インデックス配列に格納
+      problemListIndexs.push(chance.integer({ min: 0, max: problemList.length - 1 }));
+
+      // 出題順を生成
+      const problemOrder: number[] = [];
+      while (problemOrder.length < 4) {
+        const order = chance.integer({ min: 0, max: 3 });
+        // 重複していない場合
+        if (!problemOrder.includes(order)) {
+          problemOrder.push(order);
+        }
+      }
+      problemOrderList.push(problemOrder); // リストに格納
+    }
+    console.log(problemListIndexs.join(',')); // TODO: 動作確認のため追加
+
+    const problemInfos: ProblemInfo[] = []; // 問題情報配列
+    // 問題情報配列に情報を設定
+    problemListIndexs.forEach((index, i) => {
+      // 問題を取得
+      const problem = problemList[index].problem;
+      problemInfos.push({
+        problem: [
+          problem[problemOrderList[i][0]],
+          problem[problemOrderList[i][1]],
+          problem[problemOrderList[i][2]],
+          problem[problemOrderList[i][3]],
+        ],
+        answer: problemList[index].answer,
+      })
+    });
+    console.log(problemInfos);
+    return problemInfos;
   }
 
   /**
